d14aca6c7f37ed95d64295a800c337b8f0087546
EricH <AvarioDeveloper@gmail.com>
2018-08-02 18:28:03 -0600

    Fix master-atul/react-native-appstore-version-checker#7

--- src/versionChecker.ios.js
+++ src/versionChecker.ios.js
@@ -1,7 +1,7 @@
 import result from 'lodash/result';
 import {get, parseJson} from './fetcher';
 
-const getAppstoreAppVersion = (identifier, { typeOfId = 'id', country = 'us' }) => {
+const getAppstoreAppVersion = (identifier, typeOfId = 'id', country = 'us') => {
   const url = `https://itunes.apple.com/lookup?${typeOfId}=${identifier}&country=${country}`;
   return get(url).then(parseJson).then((d) => {
     const version = result(d, 'data.results[0].version');
