93efc09192b007ba0d3095112c081dcdc0bd7247
Regan Langford <regan.reihana@gmail.com>
2018-08-16 10:15:20 +1200

    EDIT replace lodash with lodash.result, add no cache params to fectch and make country optional an optional param in iOS

--- src/versionChecker.ios.js
+++ src/versionChecker.ios.js
@@ -1,10 +1,11 @@
-import result from 'lodash/result';
+import _result from 'lodash.result';
 import {get, parseJson} from './fetcher';
 
 const getAppstoreAppVersion = (identifier, options = { typeOfId: 'id', country: 'us' }) => {
-  const url = `https://itunes.apple.com/lookup?${options.typeOfId}=${identifier}&country=${options.country}`;
+  const country = options.country ? `&country=${options.country}` : '';
+  const url = `https://itunes.apple.com/lookup?${options.typeOfId}=${identifier}${country}`;
   return get(url).then(parseJson).then((d) => {
-    const version = result(d, 'data.results[0].version');
+    const version = _result(d, 'data.results[0].version');
     if (!version) {
       throw new Error('App not found!');
     }
