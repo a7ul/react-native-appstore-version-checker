2c66bb6ad04f2bc7a4cd423f88b7b85ebb38b852
David Saltares <david.saltares@gmail.com>
2019-11-13 10:20:50 +0200

    feature: addGetAppstoreAppMetadata method that includes release date

--- android/src/main/java/com/masteratul/RNAppstoreVersionCheckerModule.java
+++ android/src/main/java/com/masteratul/RNAppstoreVersionCheckerModule.java
@@ -5,6 +5,9 @@ import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.Arguments;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -22,23 +25,40 @@ public class RNAppstoreVersionCheckerModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void appVersionExtractor(
-            String url, String extractor,
+    public void extractMetadata(
+            String url, ReadableMap extractors,
             Callback successCallback,
             Callback errorCallback) {
         try {
             Document doc = Jsoup.connect(url).get();
-            String versionText;
-            if(extractor.equalsIgnoreCase("")){
-                versionText = doc.getElementsContainingOwnText("Current Version").parents().first().getAllElements().last().text();
-            }else{
-              versionText = doc.select(extractor).text();
-            }
+            WritableMap metadata = Arguments.createMap();
+
+            metadata.putString(
+                "version",
+                extractField(doc, "Current Version", "version", extractors)
+            );
+            metadata.putString(
+                "currentVersionReleaseDate",
+                extractField(doc, "Updated", "currentVersionReleaseDate", extractors)
+            );
+
             // https://stackoverflow.com/a/49924787/2881112
-            successCallback.invoke(versionText);
+            successCallback.invoke(metadata);
         } catch (Exception e) {
             errorCallback.invoke(e.getMessage());
         }
 
+    }
+
+    private String extractField(
+            Document doc,
+            String fieldLabel,
+            String extractorName,
+            ReadableMap extractors) {
+        if(extractors.hasKey(extractorName)) {
+            return doc.select(extractors.getString(extractorName)).text();
+        }
+
+        return doc.getElementsContainingOwnText(fieldLabel).parents().first().getAllElements().last().text();
     }
 }
--- src/versionChecker.android.js
+++ src/versionChecker.android.js
@@ -1,13 +1,34 @@
 import {NativeModules} from 'react-native';
-import {get} from './fetcher';
-const getAppstoreAppVersion = (id, options = { jquerySelector: "" }) => {
 
-  const url = `https://play.google.com/store/apps/details?id=${id}`;
-  return new Promise((resolve, reject) => {
-    NativeModules.RNAppstoreVersionChecker.appVersionExtractor(url,options.jquerySelector,resolve, reject);
-  });
+const extractMetadata = (url, jquerySelectors) => new Promise((resolve, reject) => {
+  NativeModules.RNAppstoreVersionChecker.extractMetadata(
+    url,
+    jquerySelectors,
+    resolve,
+    reject,
+  );
+});
+
+const getAppstoreAppMetadata = async (id, options = { jquerySelectors: {} }) => {
+  const language = options.language ? `&hl=${options.language}` : 'en';
+  const url = `https://play.google.com/store/apps/details?id=${id}&${language}`;
+  const { version, currentVersionReleaseDate } = await extractMetadata(url, options.jquerySelectors);
+  return {
+    version,
+    currentVersionReleaseDate: new Date(currentVersionReleaseDate),
+  };
+}
+
+const getAppstoreAppVersion = async (id, options = { jquerySelector: "" }) => {
+  const newOptions = {
+    jquerySelectors: { version: options.jquerySelector },
+    ...options,
+  };
+  const metadata = await getAppstoreAppMetadata(id, newOptions);
+  return metadata.version;
 };
 
 module.exports = {
-  getAppstoreAppVersion
+  getAppstoreAppVersion,
+  getAppstoreAppMetadata,
 };
